b24aacf8fe7b3d6b4248688733ddf0771e26eafb
LamyaRaed <lamyaaraed24@gmail.com>
2021-01-11 18:59:17 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pprint as pprint
+
+# todo 1: read training data (input and output vectors) [ i think we can use lists]
+# todo 2: create neurals
+# todo for program 1:
+#           todo a: Normalize features
+#           todo b: calculate values at each neural (a-s)
+#           todo c: calculate MSE
+#           todo d: back propagation algorithm and equation
+#           todo e: gradient descent
+#           todo f: stop when reaching an acceptable MSE [[[OR]]] 500 itr
+#           todo g: print MSE
+#           todo h: save final weights to file
+
+# todo for program 2:
+#           todo a: read input file
+#           todo b: read file from program 1
+#           todo c: apply feedforward propagation
+#           todo d: calculate MSE
+#           todo e: print MSE
+
+
+def main():
+    print("here")
+
+
+main()
