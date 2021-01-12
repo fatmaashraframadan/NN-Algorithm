ce9da96030996f7e570a54a7c8437e4a0697d956
LamyaRaed <lamyaaraed24@gmail.com>
2021-01-13 01:01:36 +0200

    final

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import pprint as pprint
 import random as rand
 
 
-# todo 1: read training data (input and output vectors) [ i think we can use lists]
-# todo 2: create neurons                                                                    -----DONE
+# todo 1: read training data (input and output vectors) [ i think we can use lists]         ----DONE
+# todo 2: create neurons                                                                    ----DONE
 # todo for program 1:
 #           todo a: Normalize features                                                      ----DONE
 #           todo b: calculate values at each neuron (a-s)                                   ----DONE
@@ -19,11 +19,11 @@ import random as rand
 #           todo h: save final weights to file                                              ----DONE
 
 # todo for program 2:
-#           todo a: read input file
-#           todo b: read file from program 1
-#           todo c: apply feedforward propagation
-#           todo d: calculate MSE
-#           todo e: print MSE
+#           todo a: read input file                                                         ----DONE
+#           todo b: read file from program 1                                                ----DONE
+#           todo c: apply feedforward propagation                                           ----DONE
+#           todo d: calculate MSE                                                           ----DONE
+#           todo e: print MSE                                                               ----DONE
 
 
 # feed-forward Propagation
@@ -103,12 +103,64 @@ def MSE(training_data, new_out):
     for row in range(len(training_data)):
         error = 0
         for neuron in range(input_neurons, input_neurons+out_neurons):
-            error += (1.0 / 2) * pow(training_data.iloc[i, neuron] - new_out[i*out_neurons+neuron], 2)
+            error += (1.0 / 2) * pow(training_data.iloc[row, neuron] - new_out[row], 2)
         MSE_errors.append(error)
     training_example_MSE = np.mean(MSE_errors)
     return training_example_MSE
 
 
+# Part2:  Testing
+def testing():
+    # input testing data
+    testing_data = pd.read_csv("train.txt", header=None, delim_whitespace=True, skiprows=2)
+
+    # input weights
+    # testing_input_hidden_weights = np.loadtxt('weights.txt', skiprows= 2*input_neurons+2,max_rows=input_neurons)
+    # testing_hidden_out_weights = np.loadtxt('weights.txt', skiprows=2*input_neurons+2+input_neurons, usecols=out_neurons-1)
+    testing_input_hidden_weights = input_hidden_weights
+    testing_hidden_out_weights = hidden_out_weights
+
+    #  Testing Input Data Normalization
+    for i in range(input_neurons):
+        neuron_mean = np.loadtxt("means.txt", skiprows=i, max_rows=1)
+        neuron_sd = np.loadtxt("standards.txt", skiprows=i, max_rows=1)
+        for y in range(testing_data.shape[0]):
+            testing_data.loc[y, i] = (testing_data.loc[y, i] - neuron_mean) / neuron_sd
+
+    #  Testing Output Data Normalization
+    for x in range(input_neurons, input_neurons + out_neurons):
+        max_value = np.loadtxt("max.txt", skiprows=x - input_neurons, max_rows=1)
+        min_value = np.loadtxt("min.txt", skiprows=x - input_neurons, max_rows=1)
+        for cell in range(testing_data.shape[0]):
+            testing_data.loc[cell, x] = (testing_data.loc[cell, x] - min_value) / (max_value - min_value)
+
+    # Testing Data
+    AK_testing_Predected_values = []
+    for testing_example in range(len(testing_data)):
+        # calculate forward propagation
+        Aj_testing = to_hidden(hidden_neurons, testing_data.loc[[testing_example]], testing_example,
+                               testing_input_hidden_weights)
+        Ak_testing = to_output(out_neurons, Aj_testing, testing_hidden_out_weights)
+        AK_testing_Predected_values.extend(Ak_testing)
+
+    MSE_value = MSE(testing_data, AK_testing_Predected_values)
+    print("testing MSE value : ")
+    print(MSE_value)
+
+    # Denormalize Output Data
+    denormalized_Predected_values = []
+    counter = 0
+    for row in range(len(testing_data)):
+        for x in range(input_neurons, input_neurons + out_neurons):
+            max_value = np.loadtxt("max.txt", skiprows=x - input_neurons, max_rows=1)
+            min_value = np.loadtxt("min.txt", skiprows=x - input_neurons, max_rows=1)
+            denormalized_Predected_values.append(
+                (AK_testing_Predected_values[counter] + min_value) * (max_value - min_value))
+            counter += 1
+
+
+
+# training phase:
 # read neuron sizes
 line = pd.read_csv("train.txt", header=None, delim_whitespace=True, nrows=2)
 input_neurons = int(line.iloc[0, 0])
@@ -119,50 +171,48 @@ n_examples = int(line.iloc[1, 0])
 # input training data
 training_data = pd.read_csv("train.txt", header=None, delim_whitespace=True, skiprows=2)
 
-#  Data Normalization
-file1 = open("mean_values.txt", "w")
-file1.write("mean \t std \n")
+# initialize output files
+f_means = open("means.txt", "a")
+f_standards = open("standards.txt", "a")
+f_max = open("max.txt", "a")
+f_min = open("min.txt", "a")
+f_weights = open("weights.txt", "a")
 
+
+#  Input Data Normalization
 for i in range(input_neurons):
     neuron_mean = np.mean(training_data[i])
     neuron_sd = np.std(training_data[i])
-    file1.write(str(neuron_mean))
-    file1.write("\t")
-    file1.write(str(neuron_sd))
-    file1.write("\n")
+    f_means.write('%7.9f' % neuron_mean)
+    f_means.write('\n')
+    f_standards.write('%7.9f' % neuron_sd)
+    f_standards.write('\n')
     for y in range(training_data.shape[0]):
         training_data.loc[y, i] = (training_data.loc[y, i] - neuron_mean) / neuron_sd
 
-# print("first 5 actual values")
-# print(training_data.loc[0, 8])
-# print(training_data.loc[1, 8])
-# print(training_data.loc[2, 8])
-# print(training_data.loc[3, 8])
-# print(training_data.loc[4, 8])
-
+# Output Data Normalization
 for x in range(input_neurons, input_neurons + out_neurons):
     max_value = np.max(training_data[x])
     min_value = np.min(training_data[x])
+    f_max.write('%7.9f' % max_value)
+    f_max.write('\n')
+    f_min.write('%7.9f' % min_value)
+    f_min.write('\n')
     for cell in range(training_data.shape[0]):
         training_data.loc[cell, x] = (training_data.loc[cell, x] - min_value) / (max_value - min_value)
 
-# print("first 5 actual normalize values")
-# print(training_data.loc[0, 8])
-# print(training_data.loc[1, 8])
-# print(training_data.loc[2, 8])
-# print(training_data.loc[3, 8])
-# print(training_data.loc[4, 8])
 # create 2D arrays for weights randomly
 input_hidden_weights = np.random.rand(input_neurons, hidden_neurons)
 hidden_out_weights = np.random.rand(hidden_neurons, out_neurons)
 learning_rate = 0.1
 
-AK_Predected_values = []
-#
-# print("5 predicted values")
-
 # Part1-  Training
-for itr in range(500):
+
+AK_Predected_values = []
+MSE_iterations = []
+MSE_value = 0.0
+itr = 0
+while MSE_value > 0.0005 or itr < 500:
     for example in range(n_examples):
         # calculate forward propagation
         Aj = to_hidden(hidden_neurons, training_data.loc[[example]], example, input_hidden_weights)
@@ -180,42 +230,21 @@ for itr in range(500):
         hidden_out_weights = new_out_weights
     # calculating MSE
     MSE_value = MSE(training_data, AK_Predected_values)
+    MSE_iterations.append(MSE_value)
     print(MSE_value)
-    file1.write("iteration ")
-    file1.write(str(itr + 1))
-    file1.write(" :")
-    file1.write("\t")
-    file1.write(str(MSE_value))
-    file1.write("\n")
     AK_Predected_values = []
 
-file1.close()
+print("training MSE value : ")
+print(MSE_value)
 
-file2 = open("weights.txt", "w")
-file2.write(input_hidden_weights)
-file2.write(hidden_out_weights)
-file2.close()
-# Part2-  Testing
 
-# testing_line = pd.read_csv("testing.txt", header=None, delim_whitespace=True, nrows=2)
-# testing_input_neurons = int(line.iloc[0, 0])
-# testing_hidden_neurons = int(line.iloc[0, 1])
-# testing_out_neurons = int(line.iloc[0, 2])
-# testing_n_examples = int(line.iloc[1, 0])
+# save weights to output file
+np.savetxt(f_weights, input_hidden_weights, fmt='%-7.9f')
+np.savetxt(f_weights, hidden_out_weights, fmt='%-7.9f')
 
-# input testing data
-# testing_data = pd.read_csv("train.txt", header=None, delim_whitespace=True)
-#
-# # input weights
-# testing_input_hidden_weights = pd.read_csv("weights.txt", header=None, delim_whitespace=True, nrows=input_neurons)
-# testing_hidden_out_weights = pd.read_csv("weights.txt", header=None, delim_whitespace=True, skiprows=input_neurons)
-#
-# AK_Predected_values = []
-# for testing_example in range(testing_data):
-#     # calculate forward propagation
-#     Aj = to_hidden(hidden_neurons, testing_data.loc[[testing_example]], testing_example, testing_input_hidden_weights)
-#     Ak = to_output(out_neurons, Aj, testing_hidden_out_weights)
-#     AK_Predected_values.extend(Ak)
-#
-# MSE_value = MSE(training_data, AK_Predected_values)
-# print(MSE_value)
+f_means.close()
+f_standards.close()
+f_max.close()
+f_min.close()
+f_weights.close()
+testing()
